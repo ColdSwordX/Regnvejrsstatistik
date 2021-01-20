83047ef5c136e2c17739e9995d59c3bc66c3a1f9
Jens Anker Bruun de Neergaard <jens5561@elevcampus.dk>
2021-01-20 10:49:21 +0100

    Version 1.0.0 Tilføjet Menu Tilføjet udskriving af alle værdiger.

--- Regnvejrsstatistik/Program.cs
+++ Regnvejrsstatistik/Program.cs
@@ -1,4 +1,4 @@
-﻿// Version 0.2.0
+﻿// Version 1.0.0
 
 using System;
 using System.Collections.Generic;
@@ -10,33 +10,61 @@ namespace Regnvejrsstatistik
 {
     class Program
     {
-        public static List<string> ugeDage = new List<string>() { "Mandag", "Tirsdag","Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag" };
+        public static List<string> ugeDage = new List<string>() { "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag" };
         public static double[] ugenDageVaerdier = new double[7];
         public static string[] ugeDageKommentar = new string[7];
         static void Main(string[] args)
         {
-            while (true)
+            IndtastVaerdiger();
+            bool live = true;
+            while (live)
             {
-                IndtastVaerdiger();
-                Console.WriteLine(GennemsnitUdregnet());
-                Console.WriteLine(MaksimumUdregnet());
-                Console.WriteLine(MinimumUdregnet());
-
+                UdskrivMenu();
+                live = Menu();
+                Console.ReadLine();
             }
+        }
+        static void UdskrivMenu()//Udskriver alle menu pungter.
+        {
+            Console.Clear();
+            Console.WriteLine("Her har du en lille menu.");
+            Console.WriteLine("0: Exit program.");
+            Console.WriteLine("1: udskriv alle værdiger.");
+            Console.WriteLine("2: Udskriv Gennemsnit.");
+            Console.WriteLine("3: udskriv Maksimum-værdien.");
+            Console.WriteLine("4: udskriv minimum-værdien.");
+        }
+        static bool Menu()//Menu
+        {
+            bool choise = true;
+            double nummer = ErDetEtTal(Console.ReadLine());
+            switch (nummer)
+            {
+                case 0:
+                    choise = false;
+                    break;
+                case 1:
+                    UdskrivAllevaerdier();
+                    break;
+                case 2:
+                    Console.WriteLine("\n" + GennemsnitUdregnet());
+                    break;
+                case 3:
+                    Console.WriteLine("\n" + MaksimumUdregnet());
+                    break;
+                case 4:
+                    Console.WriteLine("\n" + MinimumUdregnet());
+                    break;
+            }
+            return choise;
         }
         static void IndtastVaerdiger()//håndtere indtastning af værdiger.
         {
-            for (int i = 0; i < 7 ; i++)
+            for (int i = 0; i < 7; i++)
             {
                 Console.Clear();
                 Console.WriteLine($"\n Indtast værdigen for hvor meget regn (xx,xx)mm der er om {ugeDage[i]}");
-                bool erNummer = double.TryParse(Console.ReadLine(), out double nummer);
-                while (!erNummer)
-                {
-                    Console.WriteLine("Du skal intaste et nummer!");
-                    Console.WriteLine("Prøv igen");
-                    erNummer = double.TryParse(Console.ReadLine(), out nummer);
-                }
+                double nummer = ErDetEtTal(Console.ReadLine());
                 Console.WriteLine($"{nummer}mm blev indsat på {ugeDage[i]}");
                 ugenDageVaerdier[i] = nummer;
                 IndtastKommentar(i);
@@ -48,6 +76,14 @@ namespace Regnvejrsstatistik
             Console.WriteLine("\n Du kan skrive kommentar til dagen.");
             ugeDageKommentar[ugeDagNummer] = Console.ReadLine();
             Console.WriteLine("Kommentar tilføjet, tryk på en tast, for at forsætte.");
+        }
+        static void UdskrivAllevaerdier()//Udskriver alle værdier 
+        {
+            for (int i = 0; i < ugenDageVaerdier.Length; i++)
+            {
+                Console.WriteLine($"-\t {ugeDage[i]}\t\t {ugenDageVaerdier[i]}mm");
+                Console.WriteLine($"{ugeDageKommentar[i]} ");
+            }
         }
         static double GennemsnitUdregnet()//Finder gennemsnittet.
         {
@@ -83,6 +119,17 @@ namespace Regnvejrsstatistik
                 }
             }
             return minimum;
+        }
+        static double ErDetEtTal(string indtastet)//Finder ud af om det er et tal. Hvis det IKKE er, tvinges brugeren til at skrive et.
+        {
+            bool erNummer = double.TryParse(indtastet, out double nummer);
+            while (!erNummer)
+            {
+                Console.WriteLine("Du skal intaste et nummer!");
+                Console.WriteLine("Prøv igen");
+                erNummer = double.TryParse(Console.ReadLine(), out nummer);
+            }
+            return nummer;
         }
     }
 }
